3b1f6b1a37b26d00bea41e4b7c14e099caac1a13
luoxn28 <luoxn28@163.com>
2015-11-29 22:18:23 +0800

    增加显示天气信息页面，添加处理JSON数据方法

--- app/src/main/java/util/Utility.java
+++ app/src/main/java/util/Utility.java
@@ -1,7 +1,16 @@
 package util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import model.City;
 import model.CoderWeatherDB;
@@ -78,5 +87,45 @@ public class Utility {
         }
 
         return false;
+    }
+
+    /* 解析服务器返回的SJON数据，并将解析出的数据保存到本地，示例数据
+     * {"weatherinfo":
+     * {"city":"杭州","cityid":"101210101","temp1":"23℃","temp2":"12℃","weather":"阵雨转中雨","img1":"d3.gif","img2":"n8.gif","ptime":"08:00"}
+     * }
+     */
+    public static void handleWeatherResponse(Context context, String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+
+            String cityName = weatherInfo.getString("city");
+            String weatherCode = weatherInfo.getString("cityid");
+            String temp1 = weatherInfo.getString("temp1");
+            String temp2 = weatherInfo.getString("temp2");
+            String weatherDesp = weatherInfo.getString("weather");
+            String publishTime = weatherInfo.getString("ptime");
+            saveWeatherInfo(context, cityName, weatherCode, temp1, temp2, weatherDesp, publishTime);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // 将服务器返回的所有信息存储到SharedPreferences文件中
+    public static void saveWeatherInfo(Context context, String cityName, String weatherCode, String temp1,
+                                       String temp2, String weatherDesp, String publishTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+
+        editor.putBoolean("city_selected", true);
+        editor.putString("city_name", cityName);
+        editor.putString("weather_code", weatherCode);
+        editor.putString("temp1", temp1);
+        editor.putString("temp2", temp2);
+        editor.putString("weather_desp", weatherDesp);
+        editor.putString("publish_time", publishTime);
+        editor.putString("current_date", sdf.format(new Date()));
+        editor.commit();
     }
 }
